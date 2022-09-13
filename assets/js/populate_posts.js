698d6c66261f6da7583a421494d9d76c470159e1
adamlahbib <adem99.lhb@gmail.com>
2022-09-13 15:08:26 +0100

    added pagination for writeups

--- assets/js/populate_posts.js
+++ assets/js/populate_posts.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
     //hide first div or remove after append using `$(".card:first").remove()`
     $(".post_container:first").hide()
     $.ajax({
@@ -21,3 +21,55 @@ $(function() {
       }
     });
   });
+*/
+
+var posts="";
+
+function template(data) {
+  var html = '<ul class="post-list row" style="padding-left:0">';
+  $.each(data, function(index, item){
+      var date = item.date;
+      var blog = item.blog;
+      var title = item.title;
+      var link = item.link;
+      html += '<li class="post_container col-xs-12 col-sm-6" data-aos="fade-up" data-aos-delay="100"><span class="post-meta"><small>'+date+' / '+blog+'</small></span><h3 class="post-title" style="margin-top:12px"><a class="post-link" href="'+link+'" target="_blank">'+title+'</a></h3></li>';
+  });
+  html += '</ul>';
+  return html;
+
+  
+
+                  
+        
+
+}
+
+$.ajax({
+    url: "../../writeups/index.json",
+    success: function(result) {
+      posts=(result.posts.reverse())
+    },
+    async: false
+  });
+
+  
+  $(function(){
+    console.log(posts);
+    var container = $('#pagination-bar');
+    container.pagination({
+        dataSource: posts,
+        pageSize: 4,
+        autoHidePrevious: true,
+        autoHideNext: true,
+        callback: function(data, pagination) {
+
+
+          var html = template(data);
+          $('#pagination-data-container').html(html);
+
+        }
+    });
+    
+  });
+
+
