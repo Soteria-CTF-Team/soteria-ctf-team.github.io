6f342871699f068a2782c58dea36f800b7a590e4
Adam Lahbib <37028131+adamlahbib@users.noreply.github.com>
2022-09-15 10:54:31 +0100

    Update populate_json.js

--- assets/js/populate_json.js
+++ assets/js/populate_json.js
@@ -12,7 +12,7 @@ $(function() {
           var rank = item.place;
           //add values inside divs
           var rankclass = rank == 1 ? "rank first" : "rank";
-          $(cards).find(".card-title").html("<h2>"+ctfname+"<span><small>Points: "+score+"</small><small>Weight: "+weight+"</></span> </h2> <div class='"+rankclass+"'><h4>"+rank+"</h4></div>");
+          $(cards).find(".card-title").html("<h2>"+ctfname+"<span><small>CTF Points: "+score+"</small><small>Rating Points: "+weight+"</></span> </h2> <div class='"+rankclass+"'><h4>"+rank+"</h4></div>");
           $(cards).show() //show cards
           $(cards).appendTo($(".dashboard-cards")) //append to container
         });
